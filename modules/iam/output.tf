38c234d0bee094c55c40d67f9e2c29e721744e63
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 15:19:57 +0530

    Update output.tf

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -1,6 +1,6 @@
 output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
+  value = aws_iam_role.ecs_task_execution_role
 }
 output "ecs_task_role_arn" {
-  value = aws_iam_role.ecs_task.arn
+  value = aws_iam_role.ecs_task_role
 }
