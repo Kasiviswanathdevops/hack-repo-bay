c5f05880b7ad88215ef28877e01e2bfb478dd405
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 16:39:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "nagaraj-terraform-state-bucket"
-    key          = "usecase/statefile.tfstate"
+    bucket       = "hack-bucket-kasi"
+    key          = "hack-kasi/statefile.tfstate"
     region       = "us-west-1"
     encrypt      = true
     use_lockfile = true
