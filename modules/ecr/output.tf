55cbc7762dfb0bc109f736317a54783a463c0ecc
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 14:51:58 +0530

    Create output.tf

---
+++ modules/ecr/output.tf
@@ -0,0 +1,6 @@
+output "patient_service_repository_url" {
+  value = aws_ecr_repository.patient_service.repository_url
+}
+output "appointment_service_repository_url" {
+  value = aws_ecr_repository.appointment_service.repository_url
+}
