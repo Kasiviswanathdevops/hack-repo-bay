2a09aa0927738476fd157f00cfad4ed771d53a7b
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 13:32:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,31 @@
 terraform {
   backend "s3" {
-    bucket = "hack-bucket-kasi"
-    key    = "state/to/my/key"
-    region = "us-west-1"
+    bucket       = "kasi-hcl-bucket-uc8"
+    key          = "kasi-hcl-bucket-uc8/statefile.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = "healthcare-app"
+      ManagedBy   = "terraform"
+    }
   }
 }
