902afc660796abb182f634ddd8fa2d74aa1b3a15
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 16:23:18 +0530

    updae output

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,25 +1,25 @@
-variable "project_name" {
+variable "project_namee" {
   description = "Name of the project"
   type        = string
 }
 
-variable "environment" {
+variable "environmentt" {
   description = "Environment name"
   type        = string
   default =     QA
 }
 
-variable "vpc_id" {
+variable "vpc_idd" {
   description = "ID of the VPC"
   type        = string
 }
 
-variable "public_subnets" {
+variable "public_subnet" {
   description = "List of public subnet IDs"
   type        = list(string)
 }
 
-variable "security_groups" {
+variable "security_groupss" {
   description = "List of security group IDs for ALB"
   type        = list(string)
 }
