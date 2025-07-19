73e8af669453b7422737acbf176f742ed9207aa1
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 11:06:10 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,77 @@
 
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.pub_cidr
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.pub_cidr
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "public_ipv4" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
+resource "aws_security_group" "private" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "private_ipv4" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
