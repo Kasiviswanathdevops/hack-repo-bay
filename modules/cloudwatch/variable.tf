72491dd783548e7b9f8d059a6a842469a386ace8
Kasiviswanathdevops <kaiviswanathdevops@gmail.com>
2025-07-19 13:09:43 +0530

    Update variable.tf

--- modules/cloudwatch/variable.tf
+++ modules/cloudwatch/variable.tf
@@ -1,1 +1,15 @@
 
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+}
